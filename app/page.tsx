8519bc01dd967684512058aa0052478160c767a0
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2024-09-03 06:39:27 +0800

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "primereact/button";
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button label="World!" icon="pi pi-check" />
+    </div>
+  );
+}
